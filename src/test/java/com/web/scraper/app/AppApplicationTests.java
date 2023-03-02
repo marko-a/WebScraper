4a254a07c38b41b4f8102526580a9823924fee86
Marko Aligrudic <marko_aligrudic@yahoo.com>
2023-03-02 19:16:53 +0100

    setup

---
+++ src/test/java/com/web/scraper/app/AppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.web.scraper.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
